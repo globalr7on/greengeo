387dcae613caa1bb02365f1f3582a1b52b3da2e7
Jose De Freitas <josedefreitas91@gmail.com>
2022-05-08 21:45:37 -0500

    uncomment route

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 Auth::routes();
 
 Route::get('/home', 'App\Http\Controllers\HomeController@index')->name('home')->middleware('auth');
