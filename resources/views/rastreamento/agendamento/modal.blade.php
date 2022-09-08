5459ba108396c4d7d3c2e55d7aae548d9cb6184f
Victor <globalr7on@gmail.com>
2022-09-08 17:40:15 -0300

    Agendamento

--- resources/views/rastreamento/agendamento/modal.blade.php
+++ resources/views/rastreamento/agendamento/modal.blade.php
@@ -7,29 +7,8 @@
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
-
       <div class="modal-body p-2">
-        <div class="bs-stepper">
-          <div class="bs-stepper-header" role="tablist">
-            <!-- your steps here -->
-            <div class="step" data-target="#step1">
-              <button type="button" class="step-trigger" role="tab" aria-controls="step1" id="step1-trigger">
-                <span class="bs-stepper-circle bg-primary">1</span>
-              </button>
-            </div>
-            <div class="line"></div>
-            <div class="step" data-target="#step2">
-              <button type="button" class="step-trigger" role="tab" aria-controls="step2" id="step2-trigger">
-                <span class="bs-stepper-circle bg-primary">2</span>
-              </button>
-            </div>
-          </div>
-
           <form id="formAgenda" class="mb-0">
-            <div class="bs-stepper-content">
-              <!-- your steps content here -->
-              <div id="step1" class="content" role="tabpanel" aria-labelledby="step1-trigger">
-                <h4 class="text-primary font-weight-bold text-uppercase">Informações Básicas</h4>
                 <div class="row mx-0 mb-4">
                   <div class="col-md-4 text-center">
                     <div class="form-group m-0 p-0">
@@ -48,7 +27,6 @@
                       <select id="input_destinador_id" data-style="btn-warning text-white" title="Selecione"></select>
                     </div>
                   </div>
-
                   <div class="col-md-4 text-center">
                     <div class="form-group m-0 p-0">
                       <label for="input_acondicionamento_id" class="display-inherit mb-0">Acondicionamento</label>
@@ -56,7 +34,6 @@
                     </div>
                   </div>
                 </div>
-
                 <div class="row mx-0 mb-4">
                   <div class="col-md-4 align-self-center">
                     <div class="form-group mb-0">
@@ -64,7 +41,6 @@
                       <input type="text" class="form-control datetimepicker" id="input_data_inicio_coleta">
                     </div>
                   </div>
-
                   <div class="col-md-4 align-self-center">
                     <div class="form-group mb-0">
                       <label for="input_data_final_coleta">Data Final Coleta</label>
@@ -72,7 +48,6 @@
                     </div>
                   </div>
                 </div>
-
                 <div class="row mx-0 mb-4 d-none" id="motoristaDiv">
                   <div class="col-md-4 text-center">
                     <div class="form-group m-0 p-0">
@@ -80,7 +55,6 @@
                       <select id="input_veiculo_id" data-style="btn-warning text-white" title="Selecione"></select>
                     </div>
                   </div>
-
                   <div class="col-md-4 text-center">
                     <div class="form-group m-0 p-0">
                       <label for="input_motorista_id" class="display-inherit mb-0">Motorista</label>
@@ -88,17 +62,20 @@
                     </div>
                   </div>
                 </div>
-                    
-                <div class="row mx-0">
-                  <button class="btn btn-primary stepper-next">Próximo</button>
+                <h5 class="text-primary text-uppercase"><strong>Adicionar Produtos</string></h5>
+                <div id="produtoDetailDiv">
+                <div class="form-row mx-0 mb-4">
+                <div class="col-md-12">
+                  <div class="form-group">
+                    <input type="hidden" id="input_position">
+                    <select name="materiais" id="materiais" style="width: 100%">
+                      <option></option>
+                    </select>
+                  </div>
                 </div>
               </div>
-
-              <div id="step2" class="content" role="tabpanel" aria-labelledby="step2-trigger">
-                <h4 class="text-primary font-weight-bold text-uppercase">Produtos</h4>
-                <div id="produtoDetailDiv">
                   <div class="row mx-0 mb-4">
-                    <div class="col-md-3">
+                    <!-- <div class="col-md-3">
                       <div class="form-group mb-0">
                         <label for="input_codigo">Codigo</label>
                         <input type="text" class="form-control" id="input_codigo">
@@ -107,43 +84,40 @@
                         <input type="hidden" id="produto_id">
                       </div>
                     </div>
-
                     <div class="col-md-7">
                       <div class="form-group mb-0">
                         <label for="input_descricao">Descricão</label>
                         <input type="text" class="form-control" id="input_descricao">
                       </div>
-                    </div>
+                    </div> -->
 
-                    <div class="col-md-2">
-                      <div class="form-group mb-0">
-                        <label for="input_quantidade">Quantidade</label>
-                        <input type="number" class="form-control" id="input_quantidade">
-                      </div>
-                    </div>
-                  </div>
-
-                  <div class="row mx-0 mb-4">
-                    <div class="col-md-4 text-center">
-                      <div class="form-group m-0 p-0">
-                        <label for="input_unidade_id" class="display-inherit mb-0">Unidade</label>
-                        <select id="input_unidade_id" data-style="btn-warning text-white" title="Selecione"></select>
-                      </div>
-                    </div>
-
-                    <div class="col-md-4 align-self-center">
+                    <div class="col-md-3 align-self-center">
                       <div class="form-group mb-0">
                         <label for="input_peso">Peso</label>
                         <input type="text" class="form-control" id="input_peso">
                       </div>
                     </div>
-
-                    <div class="col-md-4 align-self-center text-center">
+                    <div class="col-md-4">
+                      <div class="form-group mb-0">
+                        <label for="input_quantidade">Quantidade</label>
+                        <input type="number" class="form-control" id="input_quantidade">
+                      </div>
+                    </div>
+                    <div class="col-md-3 text-center">
+                      <div class="form-group m-0 p-0">
+                        <label for="input_unidade_id" class="display-inherit mb-0">Unidade</label>
+                        <select id="input_unidade_id" data-style="btn-warning text-white" title="Selecione"></select>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                  <div class="row mx-0 mb-4">
+                   
+                    <div class="col-md-6 align-self-center text-center">
                       <button type="button" class="btn btn-primary" id="addProduto">Adicionar</button>
                     </div>
                   </div>
                 </div>
-
                 <div class="row mx-0 mb-4">
                   <div class="col-md-12">
                     <table class="table" id="produtosTbl">
@@ -172,13 +146,11 @@
                 </div>
 
                 <div class="row mx-0">
-                  <button class="btn btn-primary stepper-prev">Anterior</button>
                   <button type="button" class="btn btn-success" id="salvarAgenda">Salvar</button>
                 </div>
               </div>
-            </div>
           </form>
-        </div>
+       </div>
       </div>
     </div>
   </div>
